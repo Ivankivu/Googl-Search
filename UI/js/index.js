116188aae97a426ab4ec1a83d23d007c76d59a5c
Ivankivu <ivankivu@gmail.com>
2018-11-03 09:08:25 +0300

    Add footer with links
    [delivers #161646678]

--- UI/js/index.js
+++ UI/js/index.js
@@ -1,26 +1,8 @@
-function myFunction() {
-    var x, text;
 
-    // Get the value of the input field with id="numb"
-    x = document.getElementById("numb").value;
-
-    // If x is Not a Number or less than one or greater than 10
-    if (isNaN(x) || x < 1 || x > 10) {
-        text = "Input not valid";
-    } else {
-        text = "Input OK";
-    }
-    document.getElementById("demo").innerHTML = text;
-}
-
-
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown if the user clicks outside of it
 window.onclick = function(e) {
   if (!e.target.matches('.dropbtn')) {
     var myDropdown = document.getElementById("myDropdown");
